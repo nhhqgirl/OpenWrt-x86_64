9e97fe4695f00ccc426bff46a4484faf86a937cf
nhhqgirl <121777332+nhhqgirl@users.noreply.github.com>
2024-12-15 07:46:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,5 +10,5 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 sed -i '$a src-git smpackage https://github.com/gxnas/OpenWrt_Build_x64_Packages' feeds.conf.default
-sed -i '$a src-git strongswan https://github.com/strongswan/strongswan.git' feeds.conf.default
+# sed -i '$a src-git strongswan https://github.com/strongswan/strongswan.git' feeds.conf.default
 # sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
